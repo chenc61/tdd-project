c344a0f68350d82268e91fd3d5bd393e5845ba71
chenc <1429278462@qq.com>
2025-08-15 18:55:16 +0800

    refactor: created Money and Portfolio modules; imporved tests

---
+++ js/portfolio.js
@@ -0,0 +1,16 @@
+const Money = require('./money');
+class Portfolio{
+    constructor(){
+        this.moneys = [];
+    }
+    add(...money){
+        this.moneys = this.moneys.concat(money);
+    }
+    evaluate(currency){
+        let total = this.moneys.reduce((sum,money)=>{
+            return sum + money.amount;
+        },0)
+        return new Money(total,currency);
+    }
+}
+module.exports = Portfolio;
